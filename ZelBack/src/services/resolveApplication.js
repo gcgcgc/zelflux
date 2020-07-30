c7e97b89c4fe804ed901ae420edb8c9a4bba9ca3
TheTrunk <tadeas.kmenta@zel.cash>
2020-07-30 15:35:35 +0200

    correct await

--- ZelBack/src/services/resolveApplication.js
+++ ZelBack/src/services/resolveApplication.js
@@ -4,7 +4,7 @@ const zelappsService = require('./zelappsService');
 
 async function getAppPort(app) {
   try {
-    const specifics = zelappsService.getApplicationSpecifications(app, false);
+    const specifics = await zelappsService.getApplicationSpecifications(app, false);
     const { port } = specifics;
     return port;
   } catch (e) {
--- app.js
+++ app.js
@@ -42,12 +42,12 @@ ZelFrontApp.listen(config.server.zelfrontport, () => {
 const mainApp = express();
 mainApp.use(vhost('*.api.fluxos.network', app));
 mainApp.use(vhost('*.ui.fluxos.network', ZelFrontApp));
-mainApp.use(vhost('*.app.fluxos.network', async (req, res) => {
+mainApp.use(vhost('*.*.app.fluxos.network', async (req, res) => {
   try {
     console.log(req.vhost);
     const port = await resolveApplication.getAppPort(req.vhost['0']);
     if (port) {
-      res.redirect(`http://localhost:${port}`);
+      res.redirect(`http://${req.vhost.hostname}:${port}`);
     } else {
       res.setHeader('Content-Type', 'text/plain');
       res.end('Application unavailable');
